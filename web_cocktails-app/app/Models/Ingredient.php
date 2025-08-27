3c82fe8952bc4f056dbb4b0d93595e459146dfd4
rafa <rafabp986@outlook.com>
2025-08-27 08:10:54 +0200

    work in layouts

--- web_cocktails-app/app/Models/Ingredient.php
+++ web_cocktails-app/app/Models/Ingredient.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ingredient extends Model
 {
-        protected $table = "cocktail";
+        protected $table = "ingredients";
 
     protected $fillable = [
         "nombre",
